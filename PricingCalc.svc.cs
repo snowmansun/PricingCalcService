7624a17b8c7e39b597b9f78ebabb6aa0ee489b2c
Chen <Chen@DESKTOP-LBABRGD>
2016-12-30 16:34:48 +0800

    test

--- PricingCalc.svc.cs
+++ PricingCalc.svc.cs
@@ -40,124 +40,126 @@ namespace PricingCalcService
             //List<Products> productList = JSONStringToList<Products>(jsonProduct);
             //PricingCalcService.Products pdu = (PricingCalcService.Products)GetObjectByJson(jsonProduct, new PricingCalcService.Products());
 
-            if (modelController == null)
-            {
-                modelController = new ModelController(
-                   "PricingApiSql.SqlDbProvider, PricingApiSql",
-                   "Data Source=115.112.171.100;Initial Catalog=PricingHCCB;Persist Security Info=True;User ID=sa;Password=flowers123*",
-                   "yyyy-MM-dd");
-                modelController.CustomerExtraFields = "CCC_DISC_IND,CCC_ROUNDRLS,ALAND/TAXK1";
-                modelController.ProductExtraFields = "MTART,SCL_RET";
+            //if (modelController == null)
+            //{
+            //    modelController = new ModelController(
+            //       "PricingApiSql.SqlDbProvider, PricingApiSql",
+            //       "Data Source=115.112.171.100;Initial Catalog=PricingHCCB;Persist Security Info=True;User ID=sa;Password=flowers123*",
+            //       "yyyy-MM-dd");
+            //    modelController.CustomerExtraFields = "CCC_DISC_IND,CCC_ROUNDRLS,ALAND/TAXK1";
+            //    modelController.ProductExtraFields = "MTART,SCL_RET";
 
-                model = null;
-            }
+            //    model = null;
+            //}
 
-            if (model == null)
-            {
-                model = new Model();
-                modelController.LoadConfiguration(model);
-            }
+            //if (model == null)
+            //{
+            //    model = new Model();
+            //    modelController.LoadConfiguration(model);
+            //}
 
-            modelController.LoadCustomerCache(model,
-                 2022979,
-                 DateTime.Today, Convert.ToDateTime("9999-01-01"));
+            //modelController.LoadCustomerCache(model,
+            //     2022979,
+            //     DateTime.Today, Convert.ToDateTime("9999-01-01"));
 
-            modelController.LoadCustomer(model,
-            2022979,
-            562937,
-            DateTime.Today, Convert.ToDateTime("9999-01-01"));
+            //modelController.LoadCustomer(model,
+            //2022979,
+            //562937,
+            //DateTime.Today, Convert.ToDateTime("9999-01-01"));
 
-            // Get Process
-            PricingProcess process = model.Configuration.PricingProcesses[2];
+            //// Get Process
+            //PricingProcess process = model.Configuration.PricingProcesses[2];
 
-            // Get Customer (Cache contains single customer)
-            CustomerCache customerCache = model.CustomerCache[2022979];
+            //// Get Customer (Cache contains single customer)
+            //CustomerCache customerCache = model.CustomerCache[2022979];
 
-            PricingDoc pricingDoc = new PricingDoc();
-            pricingDoc.Customer = customerCache.Customers[562937];
-            pricingDoc.PricingDate = Convert.ToDateTime(date);
+            //PricingDoc pricingDoc = new PricingDoc();
+            //pricingDoc.Customer = customerCache.Customers[562937];
+            //pricingDoc.PricingDate = Convert.ToDateTime(date);
 
-            //json = @"ProductId":"0010651731","Quantity":1.04167,"UnitOfMeasure":"CS"},{"ProductId":"0010681531","Quantity":2.16667,"UnitOfMeasure":"CS"}";
+            ////json = @"ProductId":"0010651731","Quantity":1.04167,"UnitOfMeasure":"CS"},{"ProductId":"0010681531","Quantity":2.16667,"UnitOfMeasure":"CS"}";
 
-            int i = 10;
-            foreach (Products pdu in productList)
-            {
-                PricingItem pricingItem = new PricingItem(pricingDoc, 10);
-                pricingItem.Product = pricingDoc.Customer.ProductsByRef[pdu.ProductId];
-                pricingItem.Uom = model.Configuration.UnitOfMeasure[pdu.UnitOfMeasure];
-                pricingItem.Quantity = pdu.Quantity;
+            //int i = 10;
+            //foreach (Products pdu in productList)
+            //{
+            //    PricingItem pricingItem = new PricingItem(pricingDoc, 10);
+            //    pricingItem.Product = pricingDoc.Customer.ProductsByRef[pdu.ProductId];
+            //    pricingItem.Uom = model.Configuration.UnitOfMeasure[pdu.UnitOfMeasure];
+            //    pricingItem.Quantity = pdu.Quantity;
 
-                pricingDoc.PricingItems.Add(i, pricingItem);
-                i += 10;
-            }
+            //    pricingDoc.PricingItems.Add(i, pricingItem);
+            //    i += 10;
+            //}
 
-            bool isAssort = modelController.GetAssortmentInfo(process, pricingDoc, model);
+            //bool isAssort = modelController.GetAssortmentInfo(process, pricingDoc, model);
 
-            // Execute Calculation
-            process.ExecutePricing(pricingDoc, isAssort);
+            //// Execute Calculation
+            //process.ExecutePricing(pricingDoc, isAssort);
 
-            string jsonResult = string.Empty;
-            string jsonHead = "\"Summary\": {";
-            string jsonDetail = "\"Products\":[";
+            //string jsonResult = string.Empty;
+            //string jsonHead = "\"Summary\": {";
+            //string jsonDetail = "\"Products\":[";
 
-            foreach (var item in pricingDoc.Outputs)
-            {
-                putJson(ref jsonHead, item.Value.PricingOutput.OutputName, item.Value.Amount != 0 ? item.Value.Amount.ToString("f" + item.Value.PricingOutput.Decimals) : item.Value.Amount.ToString());
-            }
-            decimal summary_freeGoodsQty = 0;
+            //foreach (var item in pricingDoc.Outputs)
+            //{
+            //    putJson(ref jsonHead, item.Value.PricingOutput.OutputName, item.Value.Amount != 0 ? item.Value.Amount.ToString("f" + item.Value.PricingOutput.Decimals) : item.Value.Amount.ToString());
+            //}
+            //decimal summary_freeGoodsQty = 0;
 
-            foreach (var item in pricingDoc.PricingItems)
-            {
-                jsonDetail += "{";
-                PricingItem pi = item.Value;
-                putJson(ref jsonDetail, "Key", item.Key.ToString());
-                putJson(ref jsonDetail, "ProductRef", pi.Product.ProductRef);
-                putJson(ref jsonDetail, "ProductId", pi.Product.ProductId.ToString());
-                putJson(ref jsonDetail, "ProductName", pi.Product.ProductDesc);
-                putJson(ref jsonDetail, "Uom", pi.Uom.Uom);
-                putJson(ref jsonDetail, "Quantity", pi.Quantity.ToString());
-                putJson(ref jsonDetail, "TotalQuantity", pi.TotalQuantity.ToString());
+            //foreach (var item in pricingDoc.PricingItems)
+            //{
+            //    jsonDetail += "{";
+            //    PricingItem pi = item.Value;
+            //    putJson(ref jsonDetail, "Key", item.Key.ToString());
+            //    putJson(ref jsonDetail, "ProductRef", pi.Product.ProductRef);
+            //    putJson(ref jsonDetail, "ProductId", pi.Product.ProductId.ToString());
+            //    putJson(ref jsonDetail, "ProductName", pi.Product.ProductDesc);
+            //    putJson(ref jsonDetail, "Uom", pi.Uom.Uom);
+            //    putJson(ref jsonDetail, "Quantity", pi.Quantity.ToString());
+            //    putJson(ref jsonDetail, "TotalQuantity", pi.TotalQuantity.ToString());
 
-                if (pi.ItemType == PricingItem.ITEM_TYPE_FREE_GOOD)
-                {
-                    putJson(ref jsonDetail, "IsFreeGoods", "true");
-                }
-                else
-                {
-                    putJson(ref jsonDetail, "IsFreeGoods", "false");
-                }
+            //    if (pi.ItemType == PricingItem.ITEM_TYPE_FREE_GOOD)
+            //    {
+            //        putJson(ref jsonDetail, "IsFreeGoods", "true");
+            //    }
+            //    else
+            //    {
+            //        putJson(ref jsonDetail, "IsFreeGoods", "false");
+            //    }
 
-                var listNormalMarkdtScopeCode = new Dictionary<string, string>();//本品
-                var listFreeMarkdtScopeCode = new Dictionary<string, string>();//赠品
+            //    var listNormalMarkdtScopeCode = new Dictionary<string, string>();//本品
+            //    var listFreeMarkdtScopeCode = new Dictionary<string, string>();//赠品
 
-                string PriceListNo = string.Empty;
-                string SchemaNo = string.Empty;
+            //    string PriceListNo = string.Empty;
+            //    string SchemaNo = string.Empty;
 
-                foreach (var priceItem in pi.Outputs)
-                {
-                    putJson(ref jsonDetail, priceItem.Value.PricingOutput.OutputName, priceItem.Value.Amount != 0 ? priceItem.Value.Amount.ToString("f" + priceItem.Value.PricingOutput.Decimals) : "0");
+            //    foreach (var priceItem in pi.Outputs)
+            //    {
+            //        putJson(ref jsonDetail, priceItem.Value.PricingOutput.OutputName, priceItem.Value.Amount != 0 ? priceItem.Value.Amount.ToString("f" + priceItem.Value.PricingOutput.Decimals) : "0");
 
-                    //2.PriceListNo
-                    if (!string.IsNullOrEmpty(priceItem.Value.MarketScopeCode) &&
-                        priceItem.Value.MarketScopeCode.Equals("21"))
-                    {
-                        PriceListNo = priceItem.Value.RecordRef.ToString();
-                    }
-                }
+            //        //2.PriceListNo
+            //        if (!string.IsNullOrEmpty(priceItem.Value.MarketScopeCode) &&
+            //            priceItem.Value.MarketScopeCode.Equals("21"))
+            //        {
+            //            PriceListNo = priceItem.Value.RecordRef.ToString();
+            //        }
+            //    }
 
-                decimal freeGoodQty = pi.ExclFreeGoodQuantity + pi.InclFreeGoodQuantity;
-                putJson(ref jsonDetail, "FreeGoodsQty", freeGoodQty.ToString());
-                putJson(ref jsonDetail, "IsInclFreeGoods", pi.InclFreeGoodQuantity != 0 ? "true" : "false");
-                jsonDetail = jsonDetail.TrimEnd(',');
+            //    decimal freeGoodQty = pi.ExclFreeGoodQuantity + pi.InclFreeGoodQuantity;
+            //    putJson(ref jsonDetail, "FreeGoodsQty", freeGoodQty.ToString());
+            //    putJson(ref jsonDetail, "IsInclFreeGoods", pi.InclFreeGoodQuantity != 0 ? "true" : "false");
+            //    jsonDetail = jsonDetail.TrimEnd(',');
 
-                summary_freeGoodsQty += freeGoodQty;
-                jsonDetail += "},";
-            }
-            putJson(ref jsonHead, "SummaryFreeGoodsQty", summary_freeGoodsQty.ToString());
+            //    summary_freeGoodsQty += freeGoodQty;
+            //    jsonDetail += "},";
+            //}
+            //putJson(ref jsonHead, "SummaryFreeGoodsQty", summary_freeGoodsQty.ToString());
 
-            jsonHead = jsonHead.TrimEnd(',') + "},";
-            jsonDetail = jsonDetail.TrimEnd(',') + "]";
-            jsonResult = "{" + jsonHead + jsonDetail + "}";
+            //jsonHead = jsonHead.TrimEnd(',') + "},";
+            //jsonDetail = jsonDetail.TrimEnd(',') + "]";
+            //jsonResult = "{" + jsonHead + jsonDetail + "}";
+
+            string jsonResult = customerCode;
 
             return jsonResult;
         }
